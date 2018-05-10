c87897e6405d2cfd0141af47e59fa25f3757f994
bogdandey58 <bogdandey58@gmail.com>
2018-05-10 16:44:06 +0300

    c

--- js/main.js
+++ js/main.js
@@ -48,7 +48,10 @@ function showSlides(n) {
 }
 
 
+var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
 
+if(isSafari)
+    body.addClass('safari');
 
 
 
